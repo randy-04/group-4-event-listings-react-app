1399387d8c117b065d3cd5d6c9906397d29e90ea
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-23 23:11:52 +0300

    1. Add bootstrap link to index.html and index.js. 2. Loop through events useState in EventsPage. 3. Create cards in EachEvent to hold the events to be rendered. 4. Edit link to EventsPage in EventsNavBar.

--- src/components/EachEvent.js
+++ src/components/EachEvent.js
@@ -1,5 +1,27 @@
-function EachEvent() {
-    return <p>Each Event</p>
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
+
+function EachEvent({ event }) {
+    // destructuring the event prop for easier access
+    const {id, name, image, location, date, time} = event;
+    return (
+        <Card>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
+            <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <Card.Meta>{location}</Card.Meta>
+            <Card.Description>
+                {date} | {time}
+            </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+            <a>
+                
+                <Button primary><Icon name='time' />Attend</Button>
+                <Button secondary><Icon name='delete calendar' />Delete</Button>
+            </a>
+            </Card.Content>
+        </Card>
+    )
     
 }
 
