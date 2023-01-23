a75598e5bc178f9b405b15bd73de7b0cc8030f9c
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-01-24 00:25:37 +0300

    edit db.json to add a description and price add an input field in addEvent.js component

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
 function AddEvent() {
-
+    const [Event,setEvent] = useState({
+        nameof:"",
+        location:"",
+        descrption:"",
+        date:"",
+        time:"",
+        price:""
+    })
+  
     return (
         // Samuel add form here
-        <p>Form</p>
+        <div>
+            <label>Add Event</label>
+            <div>
+         <form className="align-items-centre">
+            <div className="col-sm-6 offset-sm-3 d-flex justify-content-center">
+                <br/>
+                <input className="form-control " type="text" placeholder="name" /> <br />
+                <input className="form-control" type="text" placeholder="location" /> <br />
+                <input className="form-control" type="text" placeholder="description" /> <br />
+                <input className="form-control" type="text" placeholder="time" /> <br />
+                <input className="form-control" type="numeric" placeholder="price" /> <br />
+                <input className="form-control" type="date" placeholder="date" /> <br />
+            </div>
+        </form>
+            
+            </div>
+            <button type="submit" class="btn btn-primary">Add Event</button>
+            
+
+        </div>
+       
+        
     )
     
 }
