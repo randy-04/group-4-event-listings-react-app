5ab2778fcbce8d714afecc796be1d9c06ba0d305
sophia <sophiendalo@gmail.com>
2023-01-24 15:20:39 +0300

    Edit README file Update Home component

--- README.md
+++ README.md
@@ -28,10 +28,6 @@ git@github.com:randy-04/group-4-event-listings-react-app.git
 ```
 - Your browser should open up automatically. If not, click the link you will be provided.
 
-## Technologies
-![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
-
-
 ## User Journey
 1. On opening the app,the home page will load with two tabs "Events page" and "add event". 
 2. On clicking the "Events page" tab, a list of events Will appear with an image and details on the event, each with a delete and attend button, none of which is selected at first.
@@ -43,10 +39,14 @@ git@github.com:randy-04/group-4-event-listings-react-app.git
 
 
 ## Technologies
-1.HTML
-4.BOOTSTRAP
-5.REACT
-=======
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+![Semantic UI React](https://img.shields.io/badge/Semantic%20UI%20React-%2335BDB2.svg?style=for-the-badge&logo=SemanticUIReact&logoColor=white)
+![React Router](https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
+
 ## Authors
 - [@Samuel Kuria](https://github.com/samuelkuria01)
 - [@Sophia Ndalo](https://github.com/sophie-ndalo)
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,17 @@ function Home() {
 
 
     return(
-       <section>
-        <div className="leftBox" style={{ backgroundColor: 'pink'}}>
-          <h1>EVENTS AND SHOWS</h1>
-           <p style={{ width: '30%', height: '30%', display: 'block'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+     <>
+     <section style={{backgroundSize: 'cover'}}>
+        <div className="leftBox" style={{ backgroundColor: 'pink', height: '100%', width: '40%'}}>
+          <h1 style={{ textAlign:'center'}}>EVENTS AND SHOWS</h1>
+           <p style={{ width: '100%', height: '30%', display: 'block', textAlign:"center"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
            </div>
-        <img src="https://images.pexels.com/photos/2263436/pexels-photo-2263436.jpeg?cs=srgb&dl=pexels-teddy-yang-2263436.jpg&fm=jpg" alt="" style={{ width: '100%', height: '50%' }} />
+           <img src="https://images.pexels.com/photos/2263436/pexels-photo-2263436.jpeg?cs=srgb&dl=pexels-teddy-yang-2263436.jpg&fm=jpg" alt="" style={{ width: '100%', height: '100%',}} />
         <div className="content">
        </div>
        </section>
+       </>
     )
 
     
