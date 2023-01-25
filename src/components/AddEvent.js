50212de5dab9c22ff8ab79280cf7e1b7564e8e59
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-01-26 00:20:46 +0300

    move an image from image folder to src folder modified AddEvent component added a css module named AddEvent.module.css import the module in AddEvent component use the module to style this component

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -2,15 +2,9 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
+import AddEventStyle from './AddEvent.module.css';
+import pic1 from "./senior-party.jpg"
 
-
-const heading = {
-    fontSize: '30px',
-    color: 'red',
-    textAlign: 'center',
-    display: 'flex',
-    
-}
 
 function AddEvent({onAdd}) {
     // navigator 
@@ -69,92 +63,20 @@ function AddEvent({onAdd}) {
     }
   
     return (
-        
-         <div>
-          {/* <img src="./images/plant.jpg" alt=""  style={{ width: '100%', height: '200%' }}  /> */}
-            <h1 style={heading}>Add Event</h1>
-            <div>
-         <form  className="col-sm-6 offset-sm-3" onSubmit={addEvent}>
-            <div className="col-sm-6 offset-sm-3">
-                <br/>
-                <label>
-                    Event Name:
-                    <input onChange={(e)=> handleChange(e)} className="form-control " 
-                    type="text" name="name" value={eventData.name} placeholder="name" />
-                </label> <br />
-                
-                <label>
-                    Image:
-                    <input onChange={(e)=> handleChange(e)}  className="form-control" 
-                    type="text" name="image" value={eventData.image} placeholder="image" />
-                </label> <br />
-
-                <label>
-                    Location:                
-                    <input onChange={(e)=> handleChange(e)}  className="form-control" 
-                    type="text" name="location" value={eventData.location} placeholder="location" /> 
-                </label> <br />
-
-                <label>
-                    Date:
-                    <input onChange={(e)=> handleChange(e)}   className="form-control" 
-                    type="date" name="date" value={eventData.date} placeholder="dd-mm-yyyy" /> 
-                </label> <br />
-                
-                <label>
-                    Time:
-                    <input onChange={(e)=> handleChange(e)} className="form-control" 
-                    type="time" name="time" value={eventData.time} /> 
-                </label> <br />
-
-                <label>
-                    Price:
-                    <input onChange={(e)=> handleChange(e)} className="form-control" 
-                    type="text" name="price" value={eventData.price} placeholder="price" /> 
-                </label> <br />
-               
-                <label>
-                    Description:
-                    <textarea onChange={(e)=> handleChange(e)} className="form-control w-1/2 p-1" 
-                    type="text" name="description" value={eventData.description} placeholder="description" /> 
-                </label> <br />
-
-                <label>
-                    Type of Event:
-                    <Form.Select 
-                        style={{width: "100%"}}
-                        name="type"
-                        value={eventData.type}
-                        
-                        aria-label="Default select example"
-                        
-                        onChange={(e) => {handleChange(e)}}
-                    >
-                        {/* <option value="All">Filter by Type of Event</option> */}
-                        <option value="Sherehe">Sherehe</option>
-                        <option value="Food">Food</option>
-                        <option value="Wedding">Wedding</option>
-                        <option value="Romance">Romance</option>
-                        <option value="Education">Education</option>
-                        <option value="Fashion">Fashion</option>
-                        <option value="Religious">Religious</option>
-                        <option value="Art">Art</option>
-                        <option value="Sports">Sports</option>
-                        
-                    </Form.Select>
-                </label> <br />
-                <br />
-                
-               
-                <button type="submit" className="btn btn-primary">Add Event</button>
+        <div className={AddEventStyle.container}>
+          <div  className={AddEventStyle.row}>
+            <div className={AddEventStyle.banner}>
+            <img src={pic1} alt="" className={AddEventStyle.image}></img>   
+            <div className={AddEventStyle.box}>
+            <h1 className={AddEventStyle.heading}>lorem ipsum</h1>        
             </div>
-        </form>  
-            </div> 
+             </div>
+          </div>
+
         </div>
-       
         
+
     )
-    
 }
 
 export default AddEvent;
