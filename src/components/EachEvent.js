a45ebd554e8ad264d3e886ee42353bc3901bebbf
sophia <sophiendalo@gmail.com>
2023-01-25 12:30:03 +0300

    Add delete function to EachEvent Component

--- src/components/EachEvent.js
+++ src/components/EachEvent.js
@@ -3,6 +3,21 @@ import { Card, Icon, Image, Button } from 'semantic-ui-react'
 function EachEvent({ event }) {
     // destructuring the event prop for easier access
     const {id, name, image, location, date, time, price, willAttend, description} = event;
+
+    const handleDelete = async (eventId) => {
+        try {
+          const response = await fetch(`http://localhost:3000/Events/${eventId}`, {
+            method: "DELETE",
+          });
+          const data = await response.json();
+          return data;
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+
+    
     return (
         <Card>
             {/* <Image src={image} wrapped ui={true} /> */}
@@ -33,7 +48,7 @@ function EachEvent({ event }) {
             <a>
                 
                 <Button primary><Icon name='time' />Attend</Button>
-                <Button secondary><Icon name='delete calendar' />Delete</Button>
+                <Button secondary onClick={handleDelete}><Icon name='delete calendar' />Delete</Button>
             </a>
             </Card.Content>
         </Card>
