ebd9632d179f7735c2619344bde20b35c9010202
sophia <sophiendalo@gmail.com>
2023-01-26 10:34:26 +0300

    Edit README Edit Home Component Edit Events Navbar Edit Eventspage

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ function Home() {
  return(
      <section>
         <div className="leftBox" style={{ backgroundColor: 'pink', height: '30%', width: '40%',position: "absolute", marginTop: "2%", marginLeft: "2%"}}>
-          <h1 style={{ textAlign:'center',}}>FindEvents HuB</h1>
-           <p style={{ width: '100%', height: '30%', display: 'block', textAlign:"center"}}>Your vision.Our innovation: Event solutions</p>
+          <h1 style={{ textAlign:'center',fontSize: '5rem', fontStyle: "italic"}}>FindEvents HuB</h1>
+          <p style={{ width: '100%', height: '30%', display: 'block', textAlign:"center", fontSize: '2rem' }}>"Your vision.Our innovation: Event solutions"</p>
            </div>
            <img src="https://images.pexels.com/photos/2263436/pexels-photo-2263436.jpeg?cs=srgb&dl=pexels-teddy-yang-2263436.jpg&fm=jpg" alt="" style={{ width: '100%', height: '100%'}} />
         <div className="content">
