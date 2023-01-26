4ab1425cfb44ff02d376567707e3bcf67a76c7aa
sophia <sophiendalo@gmail.com>
2023-01-26 10:43:13 +0300

    Edit Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
  return(
      <section>
         <div className="leftBox" style={{ backgroundColor: 'pink', height: '30%', width: '40%',position: "absolute", marginTop: "2%", marginLeft: "2%"}}>
-          <h1 style={{ textAlign:'center',fontSize: '5rem', fontStyle: "italic"}}>FindEvents HuB</h1>
+          <h1 style={{ textAlign:'center',fontSize: '4rem', fontStyle: "italic"}}>FindEvents HuB</h1>
           <p style={{ width: '100%', height: '30%', display: 'block', textAlign:"center", fontSize: '2rem' }}>"Your vision.Our innovation: Event solutions"</p>
            </div>
            <img src="https://images.pexels.com/photos/2263436/pexels-photo-2263436.jpeg?cs=srgb&dl=pexels-teddy-yang-2263436.jpg&fm=jpg" alt="" style={{ width: '100%', height: '100%'}} />
