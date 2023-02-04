013c0a8befd75384b0e5cd628dbffda742733402
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-04 10:50:15 +0300

    1. Add link to EventsPage within HomePage. 2. Edit homepage video. 3.  Edit position of home page video to be fixed.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
+
+import { Nav } from 'react-bootstrap';
+
 function Home() {
   return(
-      <section>
+      <section className='home-p'>
         <div className="content" style={{  height: '30%', width: '40%',position: "absolute", marginTop: "5%", marginLeft: "2%", fontSize: "160px",color: "#fff", fontWeight: "600"}}>
              <h1 style={{ textAlign:'center',fontStyle: "italic", fontSize: "160px",fontWeight: "600"}}>FindEvents HuB</h1>
-             <a style={{ textDecoration: "none",display: "inline-block", color: "#fff", fontSize: "24px", border: "2px solid #fff", padding: "14px 70px", borderRadius: "50px", marginTop: ""}} href="#">"Your vision.Our innovation: Events solutions"</a>
+             <Nav.Link style={{ textDecoration: "none",display: "inline-block", color: "#fff", fontSize: "24px", border: "2px solid #fff", padding: "14px 70px", borderRadius: "50px", marginTop: ""}} href="/eventsavailable">"Your Vision, Our Innovation: EventsHub"</Nav.Link>
         </div>
         <div>
-            <video autoPlay loop muted playsInline className="backVideo" style={{ position: 'absolute', right: '0', bottom: '0', zIndex: '-1' }}>
-                <source src="./video.mp4" type="video/mp4" style={{ width: '100%', height: '100%' }} />
+            <video autoPlay loop muted playsInline className="backVideo" style={{ position: 'fixed', right: '0', bottom: '0', zIndex: '-1' }}>
+                <source src="./videoplayback.mp4" type="video/mp4" style={{ width: '100%', height: '100%' }} />
             </video>
         </div>
         <style jsx>
